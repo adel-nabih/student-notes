13ba7ee5c216bfc40db723c37144b8b4d681850b
adel-nabih <86003399+adel-nabih@users.noreply.github.com>
2025-11-17 22:18:56 +0200

    landing page stuff

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.html',
-  styleUrl: './landing.css',
+  styleUrls: ['./landing.css']
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  goToCourses() {
+    this.router.navigate(['/courses']);
+  }
 }
